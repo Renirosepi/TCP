ef05b516c67df0d1521f742f81b414b54aa9da63
reni <rosereni95@gmail.com>
2018-03-20 14:03:51 +0530

    changed server

--- server.java
+++ server.java
@@ -7,5 +7,12 @@ public class server
 {
 public static void main(String args[]) throws Exception
 {
+Socket sock = new Socket("localhost", 5000);
+
+	InetAddress ip=sock.getInetAddress();
+
+	System.out.println(ip);       
+
+     String message1 = "Accept Best Wishes, Server";
 }
 }
