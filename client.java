548b74405b9b3b618941517232cb91a3d41e9030
Ramya <ramya1996.rp@gmail.com>
2018-03-20 15:11:20 +0530

    file updated

--- client.java
+++ client.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.io.OutputStream;
 import java.io.DataOutputStream;
 
-public class WishesClient
+public class client
 {
 	public static void main(String args[]) throws Exception
 	{
